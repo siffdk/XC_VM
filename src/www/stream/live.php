6555c7293b9b584411ba87123b9e171e04f866d5
Divarion-D <danil.dmitrievn@gmail.com>
2025-12-01 20:26:30 +0300

    Corrections after PR #45

--- src/www/stream/live.php
+++ src/www/stream/live.php
@@ -137,7 +137,7 @@ if ($rChannelInfo) {
             }
         }
     }
-    if (!$rChannelInfo["proxy"]) {
+    if (!isset($rChannelInfo["proxy"]) || !$rChannelInfo["proxy"]) {
         $rRetries = 0;
         $rPlaylist = STREAMS_PATH . $rStreamID . "_.m3u8";
         if ($rExtension == "ts") {
